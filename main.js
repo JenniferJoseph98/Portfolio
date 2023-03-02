2b0034df0b08da7492b5e1a536e21ef2bb9d8cab
JenniferJoseph98 <honeyjen1607@gmail.com>
2023-03-02 19:13:06 +0530

    My portfolio page added

---
+++ main.js
@@ -0,0 +1,14 @@
+!(function ($) {
+  "use strict";
+
+  // Preloader
+  $(window).on("load", function () {
+    if ($("#preloader").length) {
+      $("#preloader")
+        .delay(1)
+        .fadeOut("slow", function () {
+          $(this).remove();
+        });
+    }
+  });
+})(jQuery);
